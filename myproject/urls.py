f3eafbdbb163b8172105f22a684a51953080bac5
Vladimir Pavlenko <r3lo@ruqrz.com>
2026-01-18 19:39:51 +0300

    Add qth loc map

--- myproject/urls.py
+++ myproject/urls.py
@@ -6,12 +6,17 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from django.views.generic import TemplateView
+from django.views.static import serve
+from pathlib import Path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tlog.urls')),
     # Перенаправление favicon.ico на статический файл
     re_path(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.svg', permanent=True)),
+    # QTH карта - прямой доступ к файлу без /static/ в URL
+    re_path(r'^qth-loc\.html$', serve, {'path': 'qth-loc.html', 'document_root': Path(__file__).resolve().parent.parent / 'tlog' / 'static'}),
 ]
 
 # Serve media files in development
