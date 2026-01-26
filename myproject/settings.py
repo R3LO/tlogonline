06786c51cd1f1e306e202c3dfbe3548e08535eba
Vladimir Pavlenko <r3lo@ruqrz.com>
2026-01-26 18:12:18 +0300

    Settings fix

--- myproject/settings.py
+++ myproject/settings.py
@@ -16,7 +16,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'tlogonline.com', '192.168.31.5']
 
