f8f22787de9212cc170b423a9a6b8fc9d31621ec
R3LO <webz@bk.ru>
2026-02-03 22:07:09 +0300

    Cosmos fix HttpResponse

--- tlog/views/cosmos.py
+++ tlog/views/cosmos.py
@@ -1,6 +1,7 @@
 # Функции диплома Cosmos
 
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db import connection
